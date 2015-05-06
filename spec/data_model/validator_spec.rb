b529b1a31f1a15bcad633115201c52398bd7c8aa
Corey Powell <mistdragon100@gmail.com>
2015-05-06 00:37:40 -0500

    Added Validators.fetch spec

--- spec/data_model/validator_spec.rb
+++ spec/data_model/validator_spec.rb
@@ -8,6 +8,12 @@ describe Moon::DataModel::Validators do
     end
   end
 
+  context '.fetch' do
+    it 'fails if the validator by key doesn\'t exist' do
+      expect { described_class.fetch(:stupidity_checker) }.to raise_error(KeyError)
+    end
+  end
+
   context ':longer_than3' do
     it 'is registered' do
       expect(described_class.registered?(:longer_than3)).to eq(true)
