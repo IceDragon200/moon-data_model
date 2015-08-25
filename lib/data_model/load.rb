459bd1a60d4e0272bee58d4998fa0534d1d8c810
Corey Powell <mistdragon100@gmail.com>
2015-08-25 10:08:00 -0500

    Only set the default validator, if it hasn't already been set

--- lib/data_model/load.rb
+++ lib/data_model/load.rb
@@ -5,4 +5,4 @@ require 'data_model/fields'
 require 'data_model/metal'
 require 'data_model/base'
 
-Moon::DataModel::Validators::Type.default = Moon::DataModel::TypeValidators::Soft
+Moon::DataModel::Validators::Type.default ||= Moon::DataModel::TypeValidators::Soft
